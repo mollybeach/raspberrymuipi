0309c3484715b8e30297ee5e911fc3431cc5831f
mollybeach <56612434+mollybeach@users.noreply.github.com>
2020-03-03 22:24:20 -0800

    Update howtoplaysongsonsonicpi.rb
    lots of edits

--- howtoplaysongsonsonicpi.rb
+++ howtoplaysongsonsonicpi.rb
@@ -7,8 +7,31 @@ define :next_note do |n, c|
   #add smallest to the base note
   n + (c.map {|x| (note(x) - n) % 12}).min
 end
-#enter your key here:
-tune_key = [ :a4, :b4, :c4, :d4, :e4, :fs5, :g4]
+#enter your key here
+key = [:a, :b, :c, :d, :e, :f, :g]
+#major scales
+c_major = key
+f_major = key.map{|x| x === :b ? "#{x}b": x}
+g_major = key.map{|x| x === :f ? "#{x}s": x}
+d_major = g_major.map{|x| x === :c ? "#{x}s" : x}
+a_major = d_major.map{|x| x === :g ? "#{x}s" : x}
+e_major = a_major.map{|x| x === :d ? "#{x}s" : x}
+b_major = e_major.map{|x| x === :a ? "#{x}s" : x}
+#minor scales
+a_minor = key
+e_minor = g_major
+b_minor = d_major
+d_minor = f_major
+g_minor = f_major.map{|x| x === :e ? "#{x}b": x}
+c_minor = g_minor.map{|x| x === :a ? "#{x}b": x}
+f_minor = c_minor.map{|x| x === :d ? "#{x}b": x}
+#enter your octave
+octave = 3
+#enter your scale in tune key
+tune_key = f_minor.map! do |value| "#{value}#{octave}"
+end
+#prints out your scale
+puts tune_key
 #returns ring representing the chord
 define :tune do |tune, name, tuning=tune_key|
   chrd = (chord tune, name)
@@ -22,13 +45,13 @@ define :tune do |tune, name, tuning=tune_key|
   c
 end
 # input your delay between plays of chords
-define :preform do |c, d=0.1|
+define :preform do |c, d=0.3|
   in_thread do
     play_pattern_timed c.drop_while{|n| [nil,:r].include? n}, d
   end
 end
 use_debug false
-use_bpm 60
+use_bpm 70
 
 #enter your chords here:
 define :intro do
@@ -58,21 +81,21 @@ define :bridge do
   play ring((tune :c, :M),(tune :a, :M), (tune :g, :M), (tune :e, :M),
   (tune :c, :M), (tune :e, :M), (tune :a, :M), (tune :g, :M))
   sample :loop_breakbeat, rate: sample_duration(:loop_breakbeat)/2
-  sleep 0.5
   sample :drum_bass_soft
-  sleep 0.5
   sample :drum_cymbal_pedal
+  sleep 0.5
 end
 define :outro do
+  sample :elec_soft_kick
   puts "outro playing"
   ring((tune :c, :M), (tune :a, :m), (tune :g, :M), (tune :c, :M),
        (tune :c, :M), (tune :a, :m), (tune :g, :M), (tune :c, :M))
 end
-#enter single note patterns here
 define :melody do
   puts "melody playing"
   with_synth :fm do
     with_fx :lpf do
+      #enter single note patterns here
       ring(:f, :a, :g, :b, :b, :a, :a, :g, :e, :e, :g,
            :g, :b, :b, :d, :d, :c, :c, :e,
            :e, :a, :a, :b)
